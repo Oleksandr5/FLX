777032e9c242b6d581e5fb10f568474765d6d26a
Oleksandr5 <haliatkinoleksandr@gmail.com>
2019-01-31 17:41:43 +0200

    added homework6

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,18 +1,22 @@
-var a = prompt ("Enter the value a =", "0");
-var b = prompt ("Enter the value b =", "0");
-var c = prompt ("Enter the value c= ", "0");
-if (a===0 || а===null || b===null || c===null || isNaN(a) || isNaN(b) || isNaN(c)) {
-    alert ('Invalid input data');
+var a = prompt("Enter the value a =", "0").trim();
+var b = prompt("Enter the value b =", "0").trim();
+var c = prompt("Enter the value c= ", "0").trim();
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    alert('Invalid input data');
 } else {
-    var D = Math.pow(b,2)-4*a*c;
-    if (D > 0) {
-        var x1 = (-b + Math.sqrt (D))/2*a;
-        var x2 = (-b - Math.sqrt (D))/2*a;
-        alert ("Result: x1 = " + x1 + ", x2 = " + x2 + ".");
-    } else if (D === 0) {
-        var x = -b/2*a;
-        alert ("Result: x = " + x + ".");
+    if (a === "0" || a.length === 0 || b.length === 0 || c.length === 0 || a === null || b === null || c === null) {
+        alert('Invalid input data');
     } else {
-        alert ("no solution");
-    }       
+        var D = Math.pow(b, 2) - 4 * a * c;
+        if (D > 0) {
+            var x1 = (-b + Math.sqrt(D)) / 2 * a;
+            var x2 = (-b - Math.sqrt(D)) / 2 * a;
+            alert("Result: x1 = " + x1 + ", x2 = " + x2 + ".");
+        } else if (D === 0) {
+            var x = -b / 2 * a;
+            alert("Result: x = " + x + ".");
+        } else {
+            alert("no solution");
+        }
+    }
 }
--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,18 +1,22 @@
-var amount = prompt ("Enter the amount of money: ", "0");
-var discount = prompt ("Enter the discount: ", "0");
+var amount = prompt("Enter the amount of money: ", "0").trim();
+var discount = prompt("Enter the discount: ", "0").trim();
 if (isNaN(amount) || isNaN(discount)) {
-    alert ('Invalid input data');
+    alert('Invalid input data');
 } else {
-    if (amount >= 0 && amount <= 9999999 && discount >= 0 && discount <= 99) {        
-        var price = amount - amount*discount/100;
-        var saved = amount - price;
-        alert (
-            "Price without discount: " + amount + ";\n" + 
-            "Discount: " + discount + "%;\n" +
-            "Price with discount: " + Math.floor(price*100)/100 + ";\n" +
-            "Saved: " + Math.floor(saved*100)/100 + ";"
-        );            
+    if (amount.length === 0 || discount.length === 0 || amount === null || discount === null) {
+        alert('Invalid input data');
     } else {
-        alert ("Invalid input data");
+        if (amount >= 0 && amount <= 9999999 && discount >= 0 && discount <= 99) {
+            var price = amount - amount * discount / 100;
+            var saved = amount - price;
+            alert(
+                "Price without discount: " + amount + ";\n" +
+                "Discount: " + discount + "%;\n" +
+                "Price with discount: " + Math.floor(price * 100) / 100 + ";\n" +
+                "Saved: " + Math.floor(saved * 100) / 100 + ";"
+            );
+        } else {
+            alert("Invalid input data");
+        }
     }
 }
